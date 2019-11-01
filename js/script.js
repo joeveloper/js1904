b9dd322c49af527bf9aaaaea7bf1f8041767f0b5
joeveloper <bebosir@gmail.com>
2019-11-01 15:09:41 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//Code to display my name
+
+/*
+document.write("<strong>My name is Yinka</strong>");
+document.write("<strong>My name is Yinka</strong>");
+document.write("<strong>My name is Yinka</strong>");
+*/
+
+let name = "yinak";
+
+if (name) {
+}
+
+let myName = "Joe";
+let myFood = "Rice";
+let mySport = "Football";
+let myAddy = "Ikeja";
+let age = 30;
+let nigerian = true;
+
+document.write(
+  "My name is " +
+    myName +
+    ", I live at " +
+    myAddy +
+    " and I am " +
+    age +
+    " years old <br>"
+);
+document.write(
+  `My name is ${myName}, I live at ${myAddy} and I am ${age} years old`
+);
+
+console.log(3 * 10 + 20 / 5);
+
